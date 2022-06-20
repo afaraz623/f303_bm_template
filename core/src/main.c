19f6517b3ce425f51c3ca4270758bfd6557b8cfa
Faraz Ahmed <afaraz623@gmail.com>
2022-06-21 00:55:13 +0500

    blinky working on 8mhz system clock

--- core/src/main.c
+++ core/src/main.c
@@ -2,11 +2,18 @@
 
 int main(void)
 {
-  
-  while (1)
-  {
-    /* code */
-  }
+  	SET_BIT(RCC->AHBENR, RCC_AHBENR_GPIOEEN); 			// Enable GPIOE clock
+  	SET_BIT(GPIOE->MODER, GPIO_MODER_MODER8_0);			// Set GPIOE mode to general purpose output mode
+  	SET_BIT(GPIOE->OSPEEDR, GPIO_OSPEEDER_OSPEEDR8);	// Set GPIOE to high speed 
+  	SET_BIT(GPIOE->PUPDR, GPIO_PUPDR_PUPDR8_1);			// Set GPIOE to pull down
+   
+  	while (1)
+  	{
+		SET_BIT(GPIOE->BSRR, GPIO_BSRR_BS_8);	
+		for (int i = 0; i < 1000000; i++);		
+		SET_BIT(GPIOE->BSRR, GPIO_BSRR_BR_8);
+		for (int i = 0; i < 1000000; i++);		
+  	}
 
-  return 0;
+  	return 0;
 }
