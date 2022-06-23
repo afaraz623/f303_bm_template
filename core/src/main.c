6c5eb2688fdcbd41cb3ac0d229f2fc38c14175bf
Faraz Ahmed <afaraz623@gmail.com>
2022-06-23 05:11:42 +0500

    Configured mcu to 72mhz and added multiple leds

--- core/src/main.c
+++ core/src/main.c
@@ -1,19 +1,37 @@
 #include <stm32f3xx.h>
 
-int main(void)
-{
-  	SET_BIT(RCC->AHBENR, RCC_AHBENR_GPIOEEN); 			// Enable GPIOE clock
-  	SET_BIT(GPIOE->MODER, GPIO_MODER_MODER8_0);			// Set GPIOE mode to general purpose output mode
-  	SET_BIT(GPIOE->OSPEEDR, GPIO_OSPEEDER_OSPEEDR8);	// Set GPIOE to high speed 
-  	SET_BIT(GPIOE->PUPDR, GPIO_PUPDR_PUPDR8_1);			// Set GPIOE to pull down
-   
-  	while (1)
-  	{
-		SET_BIT(GPIOE->BSRR, GPIO_BSRR_BS_8);	
-		for (int i = 0; i < 1000000; i++);		
-		SET_BIT(GPIOE->BSRR, GPIO_BSRR_BR_8);
-		for (int i = 0; i < 1000000; i++);		
-  	}
 
-  	return 0;
+#define DELAY 100000
+
+int main(void)
+{   
+    uint32_t leds_set[] = {
+							GPIO_BSRR_BS_8, GPIO_BSRR_BS_9, 
+							GPIO_BSRR_BS_10, GPIO_BSRR_BS_11, 
+							GPIO_BSRR_BS_12, GPIO_BSRR_BS_13, 
+							GPIO_BSRR_BS_14, GPIO_BSRR_BS_15
+                          };
+
+    SET_BIT(RCC->AHBENR, RCC_AHBENR_GPIOEEN);
+    SET_BIT(GPIOE->MODER, GPIO_MODER_MODER8_0  | GPIO_MODER_MODER9_0  | 
+                          GPIO_MODER_MODER10_0 | GPIO_MODER_MODER11_0 | 
+                          GPIO_MODER_MODER12_0 | GPIO_MODER_MODER13_0 | 
+                          GPIO_MODER_MODER14_0 | GPIO_MODER_MODER15_0);
+    
+    while(1)
+    {
+        for(int i = 0; i < sizeof(leds_set) / sizeof(uint32_t); ++i)
+        {
+            SET_BIT(GPIOE->BSRR, leds_set[i]);
+            for(int volatile i = 0; i < DELAY; ++i);
+        }
+
+        for(int i = 0; i < sizeof(leds_set) / sizeof(uint32_t); ++i)
+        {
+            SET_BIT(GPIOE->BSRR, leds_set[i] << 16U);
+            for(int volatile i = 0; i < DELAY; ++i);
+        }   
+    }
+    
+    return 0;
 }
